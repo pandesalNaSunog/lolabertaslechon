8cb94cf133809dff7caca8cb3cb075d5192870ea
pandesalNaSunog <floresjem8@gmail.com>
2022-07-28 23:52:16 +0800

    latest

--- admin/php/createAdmin.php
+++ admin/php/createAdmin.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+    // if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
         date_default_timezone_set('Asia/Manila');
         $today = date('Y-m-d H:i:s');
         include('connection.php');
@@ -17,7 +17,7 @@
             $con->query($query) or die($con->error);
             echo 'ok';
         }
-    }else{
-        echo header('HTTP/1.1 403 Forbidden');
-    }
+    // }else{
+    //     echo header('HTTP/1.1 403 Forbidden');
+    // }
 ?>
