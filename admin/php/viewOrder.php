bfd94f95d635f1f72a08ac74029dd92f05ca5ee0
pandesalNaSunog <floresjem8@gmail.com>
2022-08-25 02:17:39 +0800

    latest

--- admin/php/viewOrder.php
+++ admin/php/viewOrder.php
@@ -50,7 +50,7 @@
                     'customer_address' => $customer_address,
                     'products' => $eachProductResponse,
                     'total_price' => $total,
-                    'created_at' => $created_at,
+                    'created_at' => date_format(date_create($created_at), "M d, Y h:i A"),
                     'updated_at' => $updated_at,
                     'order_type' => $order_type,
                     'date_and_time' => $date_and_time,
