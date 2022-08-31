90f9505fb65a7a32b55425128f85e45f3d31b2f9
pandesalNaSunog <floresjem8@gmail.com>
2022-08-31 14:27:45 +0800

    latest

--- admin/php/cancelOrder.php
+++ admin/php/cancelOrder.php
@@ -7,6 +7,33 @@
             echo $con->connect_error;
         }else{
             $productId = $_POST['globalProductId'];
+
+
+            $query = "SELECT * FROM orders WHERE id = '$productId'";
+            $orderQuery = $con->query($query) or die($con->error);
+            $orderRow = $orderQuery->fetch_assoc();
+
+            $productIdsAndQuantities = $orderRow['product_id_and_quantity'];
+
+
+            $productArray = explode("****", $productIdsAndQuantities);
+
+            foreach($productArray as $product){
+                if($product != ""){
+                    $productDetails = explode("**", $product);
+                    $productQuantity = $productDetails[1];
+                    $productId = $productDetails[0];
+                    $query = "SELECT * FROM products WHERE id = '$productId'";
+                    $productQuery = $con->query($query) or die($con->error);
+                    $productRow = $productQuery->fetch_assoc();
+                    $currentProductQuantity = $productRow['quantity'];
+
+                    $updatedQuantity = $currentProductQuantity + $productQuantity;
+                    
+                    $query = "UPDATE products SET quantity = $updatedQuantity WHERE id = '$productId'";
+                    $con->query($query) or die($con->error);
+                }
+            }
             $query = "DELETE FROM orders WHERE id='$productId'";
             $con->query($query) or die($con->error);
             echo 'success';
